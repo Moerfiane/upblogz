721bb33efa33d3815339814c9a8bb072051c6596
cameron mccloud <cameron1985a@yahoo.com>
2019-10-14 19:42:17 -0500

    index template update

--- src/main/java/org/launchcode/upblogz/WelcomeController.java
+++ src/main/java/org/launchcode/upblogz/WelcomeController.java
@@ -2,14 +2,18 @@ package org.launchcode.upblogz;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("welcome")
 public class WelcomeController {
 
     @RequestMapping(value = "")
-    public String index() {
-        return "Welcome Gamers";
+    public String index(Model model) {
+
+        model.addAttribute("title", "Welcome to 1Up Blogz");
+        return "welcome/index";
     }
 
 
