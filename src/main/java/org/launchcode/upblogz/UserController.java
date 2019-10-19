f88de6b991434c1ce0a7840395cfce4166c1ea92
cameron mccloud <cameron1985a@yahoo.com>
2019-10-18 21:53:09 -0500

    signup.html

--- src/main/java/org/launchcode/upblogz/UserController.java
+++ src/main/java/org/launchcode/upblogz/UserController.java
@@ -1,9 +1,27 @@
 package org.launchcode.upblogz;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("title", "signup");
+
+        return "signup/index";
+
+    }
+     String username;
+     String password;
+
+
+
+
 }
