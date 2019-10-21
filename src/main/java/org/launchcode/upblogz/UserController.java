ee9f9ea82324e813c1b868c66e988a97fbacd5b1
cameron mccloud <cameron1985a@yahoo.com>
2019-10-21 18:34:38 -0500

    application properties

--- src/main/java/org/launchcode/upblogz/UserController.java
+++ src/main/java/org/launchcode/upblogz/UserController.java
@@ -1,11 +1,11 @@
 package org.launchcode.upblogz;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import javax.validation.Valid;
 
@@ -21,20 +21,7 @@ public class UserController {
         return "signup/index";
 
     }
-     @RequestMapping(value = "add", method = RequestMethod.GET)
-     public String add(Model model) {
-        model.addAttribute(new User());
-        model.addAttribute(("title", "Create an Account");
-        return "user/add";
-     }
-     @RequestMapping(value = "add", method = RequestMethod.POST)
-     public String add(@ModelAttribute @Valid User user, errors errors, String verify, Model model) {
 
-        model.addAttribute("user", user);
-        boolean passMatch = true;
-
-
-     }
 
 
 
