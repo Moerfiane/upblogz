bec81720da69b84bd4ffd1db64df372e564cce42
cameron mccloud <cameron1985a@yahoo.com>
2019-10-22 09:49:17 -0500

    signup form

--- src/main/java/org/launchcode/upblogz/WelcomeController.java
+++ src/main/java/org/launchcode/upblogz/WelcomeController.java
@@ -6,15 +6,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("welcome")
+@RequestMapping("welcome")
 public class WelcomeController {
 
     @RequestMapping(value = "")
     public String index(Model model) {
 
         model.addAttribute("title", "Welcome to 1Up Blogz");
-        return "index";
+        return "welcome/index";
     }
+
+    @RequestMapping(value = "signup")
+    public String signupForm(Model model) {
+        model.addAttribute("title", "Sign Up");
+        return "welcome/signup";
+    }
+
 
 
 
