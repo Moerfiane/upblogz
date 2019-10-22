cfc82483f519be3fab658f919a3fd8d5a9867d1f
cameron mccloud <cameron1985a@yahoo.com>
2019-10-22 10:34:00 -0500

    html signup update

--- src/main/java/org/launchcode/upblogz/WelcomeController.java
+++ src/main/java/org/launchcode/upblogz/WelcomeController.java
@@ -1,9 +1,14 @@
 package org.launchcode.upblogz;
 
 
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("welcome")
@@ -16,11 +21,15 @@ public class WelcomeController {
         return "welcome/index";
     }
 
-    @RequestMapping(value = "signup")
+    @RequestMapping(value = "signup", method = RequestMethod.GET)
     public String signupForm(Model model) {
         model.addAttribute("title", "Sign Up");
+
+
         return "welcome/signup";
     }
+
+
 
 
 
