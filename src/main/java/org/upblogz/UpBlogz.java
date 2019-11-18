5365325a768f4eff70908da25ca45bb508db7858
Fred Hodgman <fredhodgman@gmail.com>
2019-11-17 21:09:09 -0600

    testring

--- src/main/java/org/upblogz/UpBlogz.java
+++ src/main/java/org/upblogz/UpBlogz.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class UpBlogz {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(UpBlogz.class, args);
 	}
 
 }
--- src/main/java/org/upblogz/models/data/PostDao.java
+++ src/main/java/org/upblogz/models/data/PostDao.java
@@ -1,10 +1,10 @@
 package org.upblogz.models.data;
 
 
-        import org.springframework.data.repository.CrudRepository;
-        import org.springframework.stereotype.Repository;
-        import org.upblogz.models.Post;
-        import javax.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.upblogz.models.Post;
+import javax.transaction.Transactional;
 
 @Repository
 @Transactional
